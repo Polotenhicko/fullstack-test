62f7dda946fb1ebc8a1f4bcead27613b9e4c4c01
Polo <artem-gribanof@yandex.ru>
2023-09-17 02:11:51 +0300

    cors fix

--- backend/src/constants/api.ts
+++ backend/src/constants/api.ts
@@ -1,4 +1,4 @@
 export const isDevelopment = process.env.NODE_ENV === 'development';
 export const port = process.env.PORT || 5000;
 export const allowOriginDev = 'http://localhost:3000';
-export const allowOriginProd = 'http://localhost:3000';
+export const allowOriginProd = 'https://polotenhicko.github.io/';
