7ce12cb824da43e1e66c60f3fc73baceab047fce
Polo <artem-gribanof@yandex.ru>
2023-09-17 23:18:52 +0300

    deploy

--- backend/lib/backend/src/controllers/employees.controller.js
+++ backend/lib/backend/src/controllers/employees.controller.js
@@ -1,14 +1,45 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.EmployeesController = void 0;
+const dbConnect_1 = require("../models/dbConnect");
 const models_1 = require("../models");
+const sequelize_1 = require("sequelize");
 class EmployeesController {
     constructor() {
         this.create = (req, res) => { };
         this.findAll = async (req, res) => {
             try {
-                const data = await models_1.Employees.findAll();
-                res.json(data);
+                const { offset, limit } = req.query;
+                if (!offset || !limit)
+                    throw new Error('Does not exist offset or limit!');
+                if (typeof offset !== 'string' || typeof limit !== 'string') {
+                    throw new Error('Offset or limit must be 1 arguments!');
+                }
+                const offsetNum = Number(offset);
+                const limitNum = Number(limit);
+                if (isNaN(offsetNum) || isNaN(limitNum))
+                    throw new Error('Offset or limit is not a number!');
+                const query = `
+        SELECT 
+          "employee_id" AS "employeeId",
+          "first_name" AS "firstName",
+          "last_name" AS "lastName",
+          "position",
+          "salary",
+          "hire_date" AS "hireDate",
+          "department_id" AS "departmentId" 
+        FROM "employees" 
+        AS "Employees" 
+        LIMIT :limitNum 
+        OFFSET :offsetNum
+      `;
+                const data = await dbConnect_1.sequelize.query(query, {
+                    type: sequelize_1.QueryTypes.SELECT,
+                    replacements: { limitNum, offsetNum },
+                });
+                const count = await models_1.Employees.count();
+                const hasMore = limitNum + offsetNum < count;
+                res.json({ data, hasMore });
             }
             catch (e) {
                 res.status(500).json({
