698cd74490975f7ac5eb39ca270876146e64b9f0
Polo <artem-gribanof@yandex.ru>
2023-09-20 00:05:53 +0300

    del lib/backend

--- server/lib/backend/src/constants/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.allowOriginProd = exports.allowOriginDev = exports.port = exports.isDevelopment = void 0;
-exports.isDevelopment = process.env.NODE_ENV === 'development';
-exports.port = process.env.PORT || 5000;
-exports.allowOriginDev = 'http://localhost:3000';
-exports.allowOriginProd = 'https://polotenhicko.github.io';
-//# sourceMappingURL=api.js.map
